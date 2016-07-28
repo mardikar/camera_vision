dc82bacb923086f579d5fc8a1ebb6bed138eb9a3
Saurabh Mardikar <saurabh845292@gmail.com>
2016-07-28 09:25:00 +0530

    first commit

---
+++ app/src/main/java/mardikar/saurabh/task1/cam/MainActivity.java
@@ -0,0 +1,334 @@
+package mardikar.saurabh.task1.cam;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.hardware.Camera;
+import android.hardware.Camera.PictureCallback;
+import android.hardware.Camera.ShutterCallback;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Environment;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.clarifai.api.ClarifaiClient;
+import com.clarifai.api.RecognitionRequest;
+import com.clarifai.api.RecognitionResult;
+import com.clarifai.api.Tag;
+import com.clarifai.api.exception.ClarifaiException;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity  extends Activity {
+    private Camera mCamera;
+    private CameraPreview mCameraPreview;
+    private final ClarifaiClient client = new ClarifaiClient("EEBpMsrL4IyJgCiNYcIQECtb_CpydtZsUX2K86ox",
+            "N8CxXj9ppDTDrb7Kti_61QUvfV_eGWExavF0YFYO");
+    FrameLayout preview;
+    TextView textView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mCamera = getCameraInstance();
+        List<Camera.Size> sizes=mCamera.getParameters().getSupportedPreviewSizes();
+        Camera.Size mSize=sizes.get(0);
+        for (Camera.Size size : sizes) {
+            Log.i(TAG, "Available resolution: "+size.width+" "+size.height);
+            System.out.println(size);
+            mSize = size;
+        }
+        Log.i(TAG, "Chosen resolution: " + mSize.width + " " + mSize.height);
+        mCamera.getParameters().setPreviewSize(1920, 1080);
+        Camera.Parameters parameters = mCamera.getParameters();
+        parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
+       // parameters.setFocusMode(Camera.Parameters.FLASH_MODE_AUTO);
+        mCamera.setParameters(parameters);
+        mCameraPreview = new CameraPreview(this, mCamera);
+
+        preview = (FrameLayout) findViewById(R.id.camera_preview);
+        //textView= (TextView) findViewById(R.id.image_info);
+        preview.addView(mCameraPreview);
+        textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR){
+                    textToSpeech.setLanguage(Locale.UK);
+                }
+            }
+        });
+
+
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+
+            @Override
+            public void run() {
+                // TODO Auto-generated method stub
+                takePicture();
+            }
+        }, 1000, 20000);
+    }
+
+    public void recognize(){
+        Bitmap bitmap = loadBitmapFromUri(Uri.fromFile(pictureFile));
+        System.out.println("Width : "+bitmap.getWidth()+" Height: "+bitmap.getHeight());
+        if (bitmap != null) {
+            //textView.setText("Recognizing...");
+
+
+            // Run recognition on a background thread since it makes a network call.
+            new AsyncTask<Bitmap, Void, RecognitionResult>() {
+                @Override protected RecognitionResult doInBackground(Bitmap... bitmaps) {
+                    return recognizeBitmap(bitmaps[0]);
+                }
+                @Override protected void onPostExecute(RecognitionResult result) {
+                    updateUIForResult(result);
+                }
+            }.execute(bitmap);
+        } else {
+            textView.setText("Unable to load selected image.");
+        }
+    }
+    protected void takePicture() {
+        // TODO Auto-generated method stub
+        mCamera.takePicture(shutterCallback, rawCallback, mPicture);
+    }
+
+    /**
+     * Helper method to access the camera returns null if it cannot get the
+     * camera or does not exist
+     *
+     * @return
+     */
+    private Camera getCameraInstance() {
+        Camera camera = null;
+        try {
+            camera = Camera.open();
+        }
+        catch (Exception e) {
+            // cannot get camera or does not exist
+        }
+
+        return camera;
+    }
+
+    File pictureFile;
+
+    PictureCallback mPicture = new PictureCallback() {
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+            pictureFile = getOutputMediaFile();
+            if (pictureFile == null) {
+                return;
+            }
+            try {
+                FileOutputStream fos = new FileOutputStream(pictureFile);
+                fos.write(data);
+                fos.close();
+                recognize();
+            } catch (FileNotFoundException e) {
+
+            } catch (IOException e) {
+            }
+        }
+
+    };
+
+    ShutterCallback shutterCallback = new ShutterCallback() {
+        public void onShutter() {
+            // Log.d(TAG, "onShutter'd");
+        }
+    };
+
+    PictureCallback rawCallback = new PictureCallback() {
+        public void onPictureTaken(byte[] data, Camera camera) {
+            // Log.d(TAG, "onPictureTaken - raw");
+        }
+    };
+
+    static File mediaFile;
+
+    private static File getOutputMediaFile() {
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+                "MyCameraApp");
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                Log.d("MyCameraApp", "failed to create directory");
+                return null;
+            }
+        }
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        mediaFile = new File(mediaStorageDir.getPath() + File.separator + "IMG_" + timeStamp + ".jpg");
+
+        return mediaFile;
+    }
+    String TAG = "tag : ";
+
+    public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
+        private SurfaceHolder mHolder;
+        private Camera mCamera;
+
+        public CameraPreview(Context context, Camera camera) {
+            super(context);
+            mCamera = camera;
+
+            mHolder = getHolder();
+            mHolder.addCallback(this);
+            mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        }
+
+        public void surfaceCreated(SurfaceHolder holder) {
+
+            try {
+                mCamera.setPreviewDisplay(holder);
+                mCamera.startPreview();
+                mCamera.setDisplayOrientation(90);
+            } catch (IOException e) {
+                Log.d(TAG, "Error setting camera preview: " + e.getMessage());
+            }
+        }
+
+        public void surfaceDestroyed(SurfaceHolder holder) {
+            // empty. Take care of releasing the Camera preview in your
+            // activity.
+        }
+
+        public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+            if (mHolder.getSurface() == null) {
+                // preview surface does not exist
+                return;
+            }
+
+            // stop preview before making changes
+            try {
+                mCamera.stopPreview();
+            } catch (Exception e) {
+            }
+
+            // set preview size and make any resize, rotate or
+            // reformatting changes here
+
+            // start preview with new settings
+            try {
+                mCamera.setPreviewDisplay(mHolder);
+                mCamera.startPreview();
+
+            } catch (Exception e) {
+                Log.d(TAG, "Error starting camera preview: " + e.getMessage());
+            }
+        }
+    }
+
+    /*for recognition*/
+
+    private Bitmap loadBitmapFromUri(Uri uri) {
+        try {
+            // The image may be large. Load an image that is sized for display. This follows best
+            // practices from http://developer.android.com/training/displaying-bitmaps/load-bitmap.html
+            BitmapFactory.Options opts = new BitmapFactory.Options();
+            opts.inJustDecodeBounds = true;
+            BitmapFactory.decodeStream(getContentResolver().openInputStream(uri), null, opts);
+            int sampleSize = 1;
+
+            while (opts.outWidth / (2 * sampleSize) >= preview.getWidth() &&
+                    opts.outHeight / (2 * sampleSize) >= preview.getHeight()) {
+                sampleSize *= 2;
+            }
+
+            opts = new BitmapFactory.Options();
+            opts.inSampleSize = sampleSize;
+            return BitmapFactory.decodeStream(getContentResolver().openInputStream(uri), null, opts);
+        } catch (IOException e) {
+            Log.e(TAG, "Error loading image: " + uri, e);
+        }
+        return null;
+    }
+
+    /** Sends the given bitmap to Clarifai for recognition and returns the result. */
+    private RecognitionResult recognizeBitmap(Bitmap bitmap) {
+        try {
+            // Scale down the image. This step is optional. However, sending large images over the
+            // network is slow and  does not significantly improve recognition performance.
+            Bitmap scaled = Bitmap.createScaledBitmap(bitmap, 320,
+                    320 * bitmap.getHeight() / bitmap.getWidth(), true);
+
+            // Compress the image as a JPEG.
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            scaled.compress(Bitmap.CompressFormat.JPEG, 90, out);
+            byte[] jpeg = out.toByteArray();
+
+            // Send the JPEG to Clarifai and return the result.
+            return client.recognize(new RecognitionRequest(jpeg)).get(0);
+        } catch (ClarifaiException e) {
+            Log.e(TAG, "Clarifai error", e);
+            return null;
+        }
+    }
+
+    /** Updates the UI by displaying tags for the given result. */
+    private void updateUIForResult(RecognitionResult result) {
+        if (result != null) {
+            if (result.getStatusCode() == RecognitionResult.StatusCode.OK) {
+                // Display the list of tags in the UI.
+                StringBuilder b = new StringBuilder();
+                for (Tag tag : result.getTags()) {
+                    b.append(b.length() > 0 ? ", " : "").append(tag.getName());
+                }
+                //textView.setText("Tags:\n" + b);
+                tts(b);
+            } else {
+                Log.e(TAG, "Clarifai: " + result.getStatusMessage());
+                //textView.setText("Sorry, there was an error recognizing your image.");
+            }
+        } else {
+           // textView.setText("Sorry, there was an error recognizing your image.");
+        }
+        //selectButton.setEnabled(true);
+    }
+
+    TextToSpeech textToSpeech;
+
+    void tts(StringBuilder stringBuilder){
+        textToSpeech.speak("I see there is " + stringBuilder.toString(), TextToSpeech.QUEUE_FLUSH,null);
+        deleteImage();
+    }
+
+    void deleteImage(){
+        if (mediaFile.exists()) {
+            if (mediaFile.delete()) {
+                System.out.println("file Deleted :" + mediaFile.getName());
+            } else {
+                System.out.println("file not Deleted :" + mediaFile.getName());
+            }
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        textToSpeech.stop();
+    }
+}
